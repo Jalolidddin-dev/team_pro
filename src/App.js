649a881efe4eb226e7df10e48b3ce0cb11d4aa2a
Ismoyil-hub <shaymuratovismoyil@gmail.com>
2024-11-12 18:05:05 +0500

    Ismoyil-hub

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import About from "./pages/About";
+import Ismoyil from "./pages/Ismoyil"
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-red-500">Salom</h1>
       <About />
+      <Ismoyil />
     </div>
   );
 }
---
+++ src/pages/Ismoyil.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Ismoyil() {
+  return (
+    <div>
+        <h1>Ismoyil-hub</h1>
+    </div>
+  )
+}
+
+export default Ismoyil;
