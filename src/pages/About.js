00057d9ff53cb30b80409e521a923401dfea6e28
Dew0277 <jaloliddina98@gmail.com>
2024-11-12 17:40:09 +0500

    create new Project

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/shared/Navber";
+
+function About() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default About;
