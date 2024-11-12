4e8e2d32b94c3d2b46052bbc8334de63b60ca9ba
ibrohim2o22 <jahongirovibrohim0@gmail.com>
2024-11-12 18:01:32 +0500

    ibrohim page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import About from "./pages/About";
+import Ibrohim from './pages/ibrohim'
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-red-500">Salom</h1>
       <About />
+      <Ibrohim />
     </div>
   );
 }
